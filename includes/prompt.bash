8a9c9a759ad9101da635b23e723dc66e965f86b8
Chris Jones <leeked@gmail.com>
2012-08-16 12:47:15 -0400

    Updating prompt

--- includes/prompt.bash
+++ includes/prompt.bash
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+function scm {
+    if [[ -d .git ]] || [[ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]]; then
+        SCM='git'
+    elif [[ -d .hg ]] || [[ -n "$(hg root 2> /dev/null)" ]]; then
+        SCM='hg'
+    elif [[ -d .svn ]]; then
+        SCM='svn'
+    else
+        SCM='none'
+    fi
+}
+
+function scm_prompt {
+    scm
+    [[ $SCM == 'git' ]] && git_prompt && return
+}
+
+function git_prompt {
+    local ref=$(git symbolic-ref HEAD 2> /dev/null)
+    local branch=${ref#refs/heads/}
+    if [[ -n $(git status -s 2> /dev/null | grep -v ^# | grep -v "working directory clean") ]]; then
+        echo -en "${echo_red}(${branch}) "
+    else
+        echo -en "${echo_yellow}(${branch}) "
+    fi
+}
+
 function prompt_command() {
-    # user@hostname: /path (git) $
-    PS1="${green}$SHORT_USER@\\h: ${blue}\\w ${red}"'$(__git_ps1 "(%s) ")'"${purple}\\$ ${reset_color}"
+    PS1="${green}$SHORT_USER@\\h: ${blue}\\w $(scm_prompt)${purple}\\$ ${reset_color}"
 }
 
 PROMPT_COMMAND=prompt_command;
